c2086df45a1f22261dd98651984cd66046e1ea63
romiinavega <53828146+romiinavega@users.noreply.github.com>
2019-10-15 22:24:54 -0700

    Add files via upload

---
+++ Sql/SQLQuery1.sql
@@ -0,0 +1,6 @@
+use db_simpsons; 
+
+insert into tbl_character (firstName, secondName, lastName, age, descript)
+values ('Bartolomeo','Jay', 'Simpson', 34, 'Hermano de Lisa y Maggie Simpsons');
+
+select * from tbl_character 
